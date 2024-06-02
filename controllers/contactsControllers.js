32876683a6f8825a7ef6e5671c493cdb64067f0d
izzwerg <izzwerg.kr@gmail.com>
2024-06-02 23:34:13 +0300

    step 3.2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,15 +1,28 @@
 import contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = async (req, res) => {
-    try {
-        const contacts = await contactsService.listContacts();
-        res.status(200).json(contacts);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const contacts = await contactsService.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const contact = await contactsService.getContactById(id);
+    if (contact != null) {
+      res.status(200).json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const deleteContact = (req, res) => {};
 
