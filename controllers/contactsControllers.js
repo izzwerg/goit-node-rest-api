b89e06813be6e892a628b3b4782f4795982cde4d
izzwerg <izzwerg.kr@gmail.com>
2024-06-02 23:19:30 +0300

    step 3.1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,13 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    try {
+        const contacts = await contactsService.listContacts();
+        res.status(200).json(contacts);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+};
 
 export const getOneContact = (req, res) => {};
 
